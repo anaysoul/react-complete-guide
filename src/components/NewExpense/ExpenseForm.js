5705ad37802c8e1d99f0d6e68e2d8bb251d27eff
anaysoul <soulyana_lakew@outlook.com>
2022-03-06 10:50:27 -0500

    updating state that depends on the previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -12,13 +12,20 @@ const ExpenseForm = () => {
   });
 
   const titleChangeHandler = (event) => {
-    setUserInput({ ...userInput, enteredTitle: event.target.value });
+    // setUserInput({ ...userInput, enteredTitle: event.target.value });
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
   };
   const amountChangeHandler = (event) => {
-    setUserInput({ ...userInput, enteredDate: event.target.value });
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    });
   };
   const dateChangeHandler = (event) => {
-    setUserInput({ ...userInput, enteredAmount: event.target.value });
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
   };
 
   return (
