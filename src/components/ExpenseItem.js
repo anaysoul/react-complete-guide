648b6c67be3a491a713385e64870ba502c12e0ca
anaysoul <soulyana_lakew@outlook.com>
2022-03-05 22:38:48 -0500

    adding basic css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28th 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
+      <div className="expense-item__description">
+        <h2 className="expense-item">Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
       </div>
-      <div>$294.67</div>
     </div>
   );
 }
