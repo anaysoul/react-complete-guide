6a88e64724bca36e7397f11701c9eb2aa13f4364
anaysoul <soulyana_lakew@outlook.com>
2022-03-05 22:26:37 -0500

    building a fist custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h1>Hello World, this is Soul!</h1>
+      <h4>My Second go at React!</h4>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
