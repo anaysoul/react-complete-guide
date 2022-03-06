f22a484b18990f37f28f8f35bd550c28b0afb142
anaysoul <soulyana_lakew@outlook.com>
2022-03-06 00:57:11 -0500

    a closer look at the useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,7 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
 
   const clickHandler = () => {
     setTitle('Updated!');
