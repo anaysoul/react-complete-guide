18ba80bb165b7c34f69813b38df2e3c32e97aa17
anaysoul <soulyana_lakew@outlook.com>
2022-03-06 16:35:01 -0500

    styling react components section

--- src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import './Card.css';
-
-// reusable wrapper component
-const Card = (props) => {
-  const classes = 'card ' + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
