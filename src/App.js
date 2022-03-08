fa9b55cfd3793f379a3d756768f1c49aea9e1c83
anaysoul <soulyana_lakew@outlook.com>
2022-03-07 20:08:35 -0500

    section 10 setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { useState, Fragment } from 'react';
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import React, { useState } from 'react';
 
-const App = () => {
-  const [usersList, setUsersList] = useState([]);
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { id: Math.random().toString(), name: uName, age: uAge },
-      ];
-    });
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
   };
 
   return (
-    <Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </Fragment>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
-};
+}
 
 export default App;
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-import styles from './Button.module.css';
+import classes from './Button.module.css';
 
 const Button = (props) => {
   return (
     <button
-      className={styles.button}
       type={props.type || 'button'}
+      className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
+      disabled={props.disabled}
     >
       {props.children}
     </button>
