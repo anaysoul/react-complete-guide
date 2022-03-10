22c05b5f63156fe777a1e3556d5c4e35605047e7
anaysoul <soulyana_lakew@outlook.com>
2022-03-09 21:50:22 -0500

    adding dynamic routing with params

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,15 @@
 const Products = () => {
-  return <h1>The PRODUCTS Page</h1>;
+  return (
+    <section>
+      <h1>The PRODUCTS Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Plant</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
