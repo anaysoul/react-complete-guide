280271a12c30aada5240ee1cb3d91aba7f3cfdb3
anaysoul <soulyana_lakew@outlook.com>
2022-03-09 21:16:34 -0500

    building a multi-page SPA with react router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import { useState } from 'react';
-
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
-
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
